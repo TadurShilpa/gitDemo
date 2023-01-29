7759390ea27de6b596e7973da1e0bc338c11fefa
TadurShilpa <shilpa.tadur>
2023-01-29 08:48:57 +0530

    domm.js

--- domm.js
+++ domm.js
@@ -26,16 +26,30 @@
 //header.style.borderBottom ='solid 3px #000';
 
 //GETELEMENTSBYCLASSNAME//
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent = 'Hello 2';
 //items[1].style.fontWeight = 'bold';
 //items[1].style.backgroundColor = 'yellow';
-items[3].style.backgroundColor = 'green'
-items.style.fontWeight = 'bold';
+//items[3].style.backgroundColor = 'green'
+//items.style.fontWeight = 'bold';
 
 //for(var i=0 ;i<items.length ; i++)
 //{
  //   items[i].style.backgroundColor ='#f4f4f4';
 //}
+
+//GETELEMENTBYTAGNAME
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+
+for(var i=0 ;i<li.length ; i++)
+{
+   li[i].style.backgroundColor ='#f4f4f4';
+}
